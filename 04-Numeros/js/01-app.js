f4adbc664ed2a6fca357df079ea02cc680167476
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2023-03-07 01:07:48 -0600

    Numeros, Operadores y Booleanos

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,13 @@
+//CREACION DE NUMEROS
+
+const numero = 20;
+const numero1 = "20";
+const numero2 = 20.20;
+const numero3 = .102030;
+const numero4= -20;
+
+console.log(numero);
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
---
+++ 04-Numeros/js/05-app.js
@@ -0,0 +1,13 @@
+let puntaje = 5;
+
+puntaje++; // 5
+++puntaje; // 6
+
+puntaje--; // 5
+--puntaje; // 4
+
+let puntaje2 = 5;
+
+puntaje2 -= 3;
+
+console.log(puntaje2);
