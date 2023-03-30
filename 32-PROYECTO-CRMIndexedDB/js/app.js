e5c84b9b44e1ce239f78feadecc99a579b35d9dc
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2023-03-30 16:27:08 -0600

    Proyecto 11 CRM-IndexedDB-JS

--- 32-PROYECTO-CRMIndexedDB/js/app.js
+++ 32-PROYECTO-CRMIndexedDB/js/app.js
@@ -1,5 +1,6 @@
 (function () {
   let DB;
+  const listadoClientes = document.querySelector("#listado-clientes");
 
   document.addEventListener("DOMContentLoaded", () => {
     crearDB();
@@ -7,7 +8,34 @@
     if (window.indexedDB.open("crm", 2)) {
       obtenerClientes();
     }
+
+    listadoClientes.addEventListener("click",eliminarRegistro);
+
   });
+
+
+  //Eliminar el cliente
+  function eliminarRegistro(e){
+    if(e.target.classList.contains('eliminar')){
+      const idEliminar = Number(e.target.dataset.cliente);
+      
+      const confirmar = confirm('Desea eliminar este cliente?');
+
+      if(confirmar){
+        const transaction = DB.transaction(['crm'],'readwrite');
+        const objectStore = transaction.objectStore('crm');
+        objectStore.delete(idEliminar);
+        transaction.oncomplete = function(){
+          e.target.parentElement.parentElement.remove();
+        }
+  
+        transaction.onerror= function(){
+          console.log('Hubo error');
+        }
+      };
+
+    }
+  }
 
   //Crea la bae de datos de IndexDB
   function crearDB() {
@@ -56,7 +84,6 @@
 
         if (cursor) {
           const { nombre, empresa, email, telefono, id } = cursor.value;
-          const listadoClientes = document.querySelector("#listado-clientes");
           listadoClientes.innerHTML += `<tr>
           <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
               <p class="text-sm leading-5 font-medium text-gray-700 text-lg  font-bold"> ${nombre} </p>
@@ -70,7 +97,7 @@
           </td>
           <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
               <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-              <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+              <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
           </td>
       </tr>
   `;
--- 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ 32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -1,36 +1,25 @@
 (function () {
   let DB;
-  const formulario = document.querySelector('#formulario');
+  const formulario = document.querySelector("#formulario");
 
-  document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener("DOMContentLoaded", () => {
     conectarDB();
-    formulario.addEventListener('submit', validarCliente);
+    formulario.addEventListener("submit", validarCliente);
   });
 
   //Conecta la bae de datos de IndexDB
-  function conectarDB() {
-    const conectarDB = window.indexedDB.open('crm',2);
-
-    conectarDB.onerror = function () {
-      console.log('Hubo un error');
-    };
-
-    conectarDB.onsuccess = function () {
-      DB = conectarDB.result;
-    };
-  }
 
   function validarCliente(e) {
     e.preventDefault();
 
     //Validando inputs
-    const nombre = document.querySelector('#nombre').value;
-    const email = document.querySelector('#email').value;
-    const telefono = document.querySelector('#telefono').value;
-    const empresa = document.querySelector('#empresa').value;
+    const nombre = document.querySelector("#nombre").value;
+    const email = document.querySelector("#email").value;
+    const telefono = document.querySelector("#telefono").value;
+    const empresa = document.querySelector("#empresa").value;
 
-    if (nombre === '' || email === '' || telefono === '' || empresa === '') {
-      imprimirAlerta('Todos los campos son obligatorios', 'error');
+    if (nombre === "" || email === "" || telefono === "" || empresa === "") {
+      imprimirAlerta("Todos los campos son obligatorios", "error");
       return;
     }
 
@@ -47,59 +36,20 @@
   }
 
   function crearNuevoCliente(cliente) {
-    const transaction = DB.transaction(['crm'], 'readwrite');
-    const objectStore = transaction.objectStore('crm');
+    const transaction = DB.transaction(["crm"], "readwrite");
+    const objectStore = transaction.objectStore("crm");
 
     objectStore.add(cliente);
 
     transaction.onerror = function () {
-        imprimirAlerta('Hubo Error : Crear');
+      console.log("Hubo Error : Crear");
     };
 
     transaction.oncomplete = function () {
-      imprimirAlerta('El cliente se ha agregado correctamente');
+      imprimirAlerta("El cliente se ha agregado correctamente");
       setTimeout(() => {
-        window.location.href = 'index.html';
+        window.location.href = "index.html";
       }, 3000);
     };
-  }
-
-  function imprimirAlerta(mensaje, tipo) {
-    const alerta = document.querySelector('.alerta');
-    if (!alerta) {
-      //Crear la alerta
-      const divMensaje = document.createElement('div');
-      divMensaje.classList.add(
-        'px-4',
-        'py-3',
-        'rounded',
-        'max-w-lg',
-        'mx-auto',
-        'mt-6',
-        'text-center',
-        'border',
-        'alerta'
-      );
-      if (tipo === 'error') {
-        divMensaje.classList.add(
-          'bg-red-100',
-          'border-red-400',
-          'text-red-700'
-        );
-      } else {
-        divMensaje.classList.add(
-          'bg-green-100',
-          'border-green-400',
-          'text-green-700'
-        );
-      }
-
-      divMensaje.textContent = mensaje;
-      formulario.appendChild(divMensaje);
-
-      setTimeout(() => {
-        divMensaje.remove();
-      }, 3000);
-    }
   }
 })();
