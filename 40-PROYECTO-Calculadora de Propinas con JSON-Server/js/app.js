00993149dff941c4fa0971c6ff6036ccfbb95f0e
JoanSerratoFlores <joan.serrato.flores@gmail.com>
2023-04-15 14:12:59 -0600

    Proyecto 14 Propinas-JS

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -85,13 +85,20 @@ function mostrarPlatillos(platillos) {
     const inputCantidad = document.createElement("input");
     inputCantidad.type = "number";
     inputCantidad.min = 0;
-    inputCantidad.value= 0;
+    inputCantidad.value = 0;
     inputCantidad.id = `producto-${platillo.id}`;
-    inputCantidad.classList.add('form-control');
+    inputCantidad.classList.add("form-control");
 
-    const agregar = document.createElement('div');
-    agregar.classList.add('col-md-2');
-    agregar.appendChild(inputCantidad);    
+    //funcion que detecta la cantidad y el platillo que se esta agregando
+    inputCantidad.onchange = function () {
+      const cantidad = parseInt(inputCantidad.value);
+      console.log(cantidad);
+      agregarPlatillo({ ...platillo, cantidad });
+    };
+
+    const agregar = document.createElement("div");
+    agregar.classList.add("col-md-2");
+    agregar.appendChild(inputCantidad);
 
     row.appendChild(nombre);
     row.appendChild(precio);
@@ -100,4 +107,90 @@ function mostrarPlatillos(platillos) {
 
     contenido.appendChild(row);
   });
+}
+
+function agregarPlatillo(producto) {
+  //extraer el pedido actual
+  let { pedido } = cliente;
+
+  //Revisar que la cantidad sea mayor a 0
+  if (producto.cantidad > 0) {
+    //Comprueba si el elemento ya existe en el array
+    if (pedido.some((articulo) => articulo.id === producto.id)) {
+      //El articulo ya existe, actualizar la cantidad
+      const pedidoActualizado = pedido.map((articulo) => {
+        if (articulo.id === producto.id) {
+          articulo.cantidad = producto.cantidad;
+        }
+
+        return articulo;
+      });
+
+      cliente.pedido = [...pedidoActualizado];
+    } else {
+      //El articulo no existe , lo agregamos al array de pedido
+      cliente.pedido = [...pedido, producto];
+    }
+  } else {
+    //Eliminar elementos cuando la cantidad es 0
+    const resultado = pedido.filter((articulo) => articulo.id != producto.id);
+    cliente.pedido = [...resultado];
+  }
+
+  //Limpiar el codigo HTML previo
+  limpiarHTML();
+
+  //Mostrar el resumen
+  actualizarResumen();
+}
+
+function actualizarResumen() {
+  const contenido = document.querySelector("#resumen .contenido");
+
+  const resumen = document.createElement("div");
+  resumen.classList.add("col-md-6",'card','py-5','px-3','shadow');
+
+  //Info de la mesa
+  const mesa = document.createElement("p");
+  mesa.textContent = "Mesa: ";
+  mesa.classList.add("fw-bold");
+
+  const mesaSpan = document.createElement("span");
+  mesaSpan.textContent = cliente.mesa;
+  mesaSpan.classList.add("fw-normal");
+
+  //Info de la hora
+  const hora = document.createElement("p");
+  hora.textContent = "Hora: ";
+  hora.classList.add("fw-bold");
+
+  const horaSpan = document.createElement("span");
+  horaSpan.textContent = cliente.hora;
+  horaSpan.classList.add("fw-normal");
+
+  //Agregar a los elementos padre
+  mesa.appendChild(mesaSpan);
+  hora.appendChild(horaSpan);
+
+  //Titulo de la seccion 
+  const heading = document.createElement('h3');
+  heading.textContent = 'Platillos Consumidos';
+  heading.classList.add('my-4','text-center');
+
+  //Agregar al contenido
+  resumen.appendChild(mesa);
+  resumen.appendChild(hora);
+  resumen.appendChild(heading);
+
+  contenido.appendChild(resumen);
+
+
+}
+
+function limpiarHTML(){
+  const contenido = document.querySelector('#resumen .contenido');
+
+  while(contenido.firstChild){
+    contenido.removeChild(contenido.firstChild);
+  }
 }
